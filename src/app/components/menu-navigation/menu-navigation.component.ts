27690c5831a41ec4350aa1e484355c8f0af4fd61
manosanti <luccassantiago.contato@gmail.com>
2024-10-29 16:37:11 -0300

    new-commit

--- src/app/components/menu-navigation/menu-navigation.component.ts
+++ src/app/components/menu-navigation/menu-navigation.component.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 // Variáveis Globais
-import { bbP_id, cardCode, httpOptions } from '../../global/constants';
+import { bbP_id, httpOptions } from '../../global/constants';
 import { infoBasica } from '../../models/infobasica/infobasica.model';
 
 @Component({
@@ -14,6 +14,7 @@ import { infoBasica } from '../../models/infobasica/infobasica.model';
 })
 export class MenuNavigationComponent implements OnInit {
 
+  isMenuOpen: boolean = true;
   activeBtnIndex: number | null = null;
   filter: string = 'all';
   isMenuCollapsed: boolean = false;
@@ -50,11 +51,17 @@ export class MenuNavigationComponent implements OnInit {
   ngOnInit(): void {
     const savedIndex = sessionStorage.getItem('activeBtnIndex');
     this.activeBtnIndex = savedIndex !== null ? +savedIndex : 0;
-    
+
+    const hasReloaded = sessionStorage.getItem('hasReloaded');
+
+    if (!hasReloaded) {
+      // Se o sessionStorage ainda não existir, cria e recarrega a página
+      sessionStorage.setItem('hasReloaded', 'true');
+      window.location.reload();
+    }
     // Realiza a requisição GET
     this.http.get<infoBasica[]>(`/api/BBP/BBPID?bbpid=${bbP_id}`, httpOptions).subscribe(
       response => {
-        // window.location.reload();
         if (Array.isArray(response) && response.length > 0) {
           response.forEach(data => {
             this.items.forEach(item => {
@@ -63,7 +70,7 @@ export class MenuNavigationComponent implements OnInit {
                   const grupoClientesArray = data.definir_grupos_clientes || [];
                   item.isFilled = grupoClientesArray.length > 0;
                   break;
-              
+
                 case 'Informações Básicas':
                   const site = data.site || [];
                   const email = data.email || [];
@@ -80,119 +87,119 @@ export class MenuNavigationComponent implements OnInit {
                   const XML = data.caminho_Pasta_XML || [];
                   const moedaCorrente = data.moeda_Corrente || [];
                   const moedaSistema = data.moeda_Sistema || [];
-              
+
                   item.isFilled = site.length > 0 && email.length > 0 && registroComercial.length > 0 && perfilSPED.length > 0 &&
                     dadosCTB.length > 0 && moedas.length > 0 && telefone.length > 0 && telefone2.length > 0 && word.length > 0 &&
                     excel.length > 0 && imagens.length > 0 && licencas.length > 0 && XML.length > 0 && moedaCorrente.length > 0 && moedaSistema.length > 0;
                   break;
-              
-                  case 'Configurações Gerais':
-                    const alertaAtividadeArray = data.alerta_Atividade || [];
-                    const taxaComissaoArray = data.com_base_negocio_define_taxadecomiss || [];
-                    const casasDecimaisArray = data.quantas_casas_decimais_utilizar || [];
-                    const dataArray = data.qual_formato_data_uitlizar_tela || [];
-                    const horaArray = data.qual_formato_hora_uitlizar_tela || [];
-                  
-                    // A condição correta: item.isFilled será true apenas se TODOS os arrays estiverem preenchidos
-                    const configsFilled = alertaAtividadeArray.length > 0 && 
-                                      taxaComissaoArray.length > 0 && 
-                                      casasDecimaisArray.length > 0 && 
-                                      dataArray.length > 0 && 
-                                      horaArray.length > 0;
-                  
-                    // Se todos estiverem preenchidos, isFilled será true, caso contrário será false
-                    item.isFilled = configsFilled;
-                  break;                  
-              
+
+                case 'Configurações Gerais':
+                  const alertaAtividadeArray = data.alerta_Atividade || [];
+                  const taxaComissaoArray = data.com_base_negocio_define_taxadecomiss || [];
+                  const casasDecimaisArray = data.quantas_casas_decimais_utilizar || [];
+                  const dataArray = data.qual_formato_data_uitlizar_tela || [];
+                  const horaArray = data.qual_formato_hora_uitlizar_tela || [];
+
+                  // A condição correta: item.isFilled será true apenas se TODOS os arrays estiverem preenchidos
+                  const configsFilled = alertaAtividadeArray.length > 0 &&
+                    taxaComissaoArray.length > 0 &&
+                    casasDecimaisArray.length > 0 &&
+                    dataArray.length > 0 &&
+                    horaArray.length > 0;
+
+                  // Se todos estiverem preenchidos, isFilled será true, caso contrário será false
+                  item.isFilled = configsFilled;
+                  break;
+
                 case 'Plano de Contas':
                   const planoContas = data.plano_Contas_Empresa_anexo || [];
                   item.isFilled = planoContas.length > 0;
                   break;
-              
+
                 case 'Informações de Banco':
                   const infoBanco = data.definir_Informacoes_banco || [];
                   item.isFilled = infoBanco.length > 0;
                   break;
-              
+
                 case 'Lançamentos de Transação':
                   const detVendas = data.determinacao_contacontabil_vendas || [];
                   const detGeral = data.determinacao_contacontabil_geral || [];
                   const detEstoque = data.determinacao_contacontabil_estoque || [];
                   const detCompras = data.determinacao_contacontabil_compras || [];
-              
+
                   item.isFilled = detVendas.length > 0 && detGeral.length > 0 && detEstoque.length > 0 && detCompras.length > 0;
                   break;
-              
+
                 case 'Centro de Custo':
                   const dimCentroCusto = data.definir_Dimensoes_Centros_Custo || [];
                   const centroCusto = data.definir_Centros_Custo || [];
                   item.isFilled = dimCentroCusto.length > 0 && centroCusto.length > 0;
                   break;
-              
+
                 case 'Definição de Despesas':
                   const despesa = data.definicao_Despesas || [];
                   const despAdicionais = data.despesas_Adicionais || [];
                   item.isFilled = despesa.length > 0 && despAdicionais.length > 0;
                   break;
-              
+
                 case 'Depósitos':
                   const depositos = data.defina_Depositos || [];
                   item.isFilled = depositos.length > 0;
                   break;
-              
+
                 case 'Grupo de Itens':
                   const grupoItens = data.defina_Grupo_Item || [];
                   item.isFilled = grupoItens.length > 0;
                   break;
-              
+
                 case 'Imposto':
                   const imposto = data.configuracao_Imposto_Retido_Fonte || [];
                   item.isFilled = imposto.length > 0;
                   break;
-              
+
                 case 'Condições de Pagamento':
                   const condPagamento = data.definir_condicoes_pagamentos || [];
                   item.isFilled = condPagamento.length > 0;
                   break;
-              
+
                 case 'Informações de Cartões':
                   const infoCartoes = data.definir_informacoes_cartoes || [];
                   item.isFilled = infoCartoes.length > 0;
                   break;
-              
+
                 case 'Usuários SAP':
                   const usuariosSAP = data.definir_usuario_senhas || [];
                   item.isFilled = usuariosSAP.length > 0;
                   break;
-              
+
                 case 'Territórios':
                   const territoriosGerenciar = data.definir_Territorios || [];
                   const controlarVendas = data.definir_controle_vendedor || [];
                   const percentualComissao = data.definir_grupo_comissoes || [];
                   const estagNiveisVendas = data.definir_Estagios_Niveis_Vendas || [];
-              
+
                   item.isFilled = territoriosGerenciar.length > 0 && controlarVendas.length > 0 &&
                     percentualComissao.length > 0 && estagNiveisVendas.length > 0;
                   break;
-              
+
                 case 'Grupo de Fornecedores':
                   const grupoFornecedores = data.definir_grupos_fornecedores || [];
                   item.isFilled = grupoFornecedores.length > 0;
                   break;
-              
+
                 case 'Tipos de Expedição':
                   const tiposExpedicao = data.definir_tipos_expedicao || [];
                   item.isFilled = tiposExpedicao.length > 0;
                   break;
-              
+
                 case 'Config. Inicial de Documento':
                   const configInicialDoc = data.definir_configuracoes_iniciais_documento || [];
                   item.isFilled = configInicialDoc.length > 0;
                   break;
-              
+
                 default:
                   item.isFilled = false;
-              }              
+              }
             });
           });
         } else {
@@ -204,6 +211,8 @@ export class MenuNavigationComponent implements OnInit {
         console.error('Erro ao buscar dados da API', error);
       }
     );
+    const savedStatus = sessionStorage.getItem('menuStatus');
+    this.isMenuCollapsed = savedStatus === 'collapsed'; // Define o estado com base no valor salvo  
   }
 
   setActiveBtn(index: number, link: string): void {
@@ -212,7 +221,8 @@ export class MenuNavigationComponent implements OnInit {
   }
 
   toggleMenu() {
-    this.isMenuCollapsed = !this.isMenuCollapsed;
+    this.isMenuCollapsed = !this.isMenuCollapsed; // Alterna o estado do menu colapsado
+    sessionStorage.setItem('menuStatus', this.isMenuCollapsed ? 'collapsed' : 'open'); // Salva o estado no sessionStorage
   }
 
   onFilterChange(event: Event) {
