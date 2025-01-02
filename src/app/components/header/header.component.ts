382f51e597afea4eff9baae54244d6827824882d
manosanti <luccassantiago.contato@gmail.com>
2025-01-02 12:20:50 -0300

    dark-theme-before-scss

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -15,6 +15,8 @@ export class HeaderComponent implements AfterViewInit {
   public formSearchContract: FormGroup;
   public errorMessage: string | null = null;
 
+  isDarkTheme = localStorage.getItem('isDarkTheme')! === 'true'
+
   constructor(private renderer: Renderer2, private router: Router, private http: HttpClient, private fb: FormBuilder) {
     this.formSearchContract = this.fb.group({
       cardCode: ['', Validators.required],
@@ -28,8 +30,23 @@ export class HeaderComponent implements AfterViewInit {
   }
 
   ngAfterViewInit(): void {
+    // Aplica o tema armazenado no localStorage ao carregar a página
+    document.body.classList.toggle('dark', this.isDarkTheme);
+
     const modalCard = this.renderer.selectRootElement('#modalCard', true)
     this.renderer.setStyle(modalCard, 'display', 'none')
+  }
+
+  // Função para alternar o tema
+  toggleTheme(): void {
+    // Alterna o valor do tema
+    this.isDarkTheme = !this.isDarkTheme;
+
+    // Salva o estado no localStorage
+    localStorage.setItem('isDarkTheme', String(this.isDarkTheme));
+
+    // Adiciona ou remove a classe 'dark' no body
+    document.body.classList.toggle('dark', this.isDarkTheme);
   }
 
   showSearch(): void {
--- src/app/views/login/sign-in/sign-in.component.ts
+++ src/app/views/login/sign-in/sign-in.component.ts
@@ -1,25 +1,24 @@
 import { Component, OnInit, Renderer2 } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { HeaderComponent } from '../../../components/header/header.component';
 import { FooterLoginComponent } from '../../../components/footer-login/footer-login.component';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
-import { switchMap } from 'rxjs/operators'; // Add this line
-import { CustomerFieldComponent } from '../../../components/customer-field/customer-field.component';
+import { switchMap } from 'rxjs/operators';
 import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [HeaderComponent, CustomerFieldComponent, RouterModule, FooterLoginComponent, ReactiveFormsModule, HttpClientModule, NgIf],
+  imports: [RouterModule, FooterLoginComponent, ReactiveFormsModule, HttpClientModule, NgIf],
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent implements OnInit {
   isLoading: boolean = false;
   loginForm: FormGroup;
+  isDarkTheme = localStorage.getItem('isDarkTheme')! === 'true';
 
   constructor(private fb: FormBuilder, private router: Router, private renderer: Renderer2, private http: HttpClient) {
     this.loginForm = this.fb.group({
@@ -28,7 +27,10 @@ export class SignInComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.isDarkTheme = localStorage.getItem('isDarkTheme') === 'true';
+    document.body.classList.toggle('dark', this.isDarkTheme);
+  }
 
   onSubmit() {
     const loginData = {
